d0d4fc2cb0bd8b4322a9c76b8242f232b43d9a8f
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2025-02-23 13:24:53 +0530

    Time: 201 ms (57.89%), Space: 110 MB (78.95%) - LeetHub

--- 3764-maximum-sum-with-at-most-k-elements/3764-maximum-sum-with-at-most-k-elements.cpp
+++ 3764-maximum-sum-with-at-most-k-elements/3764-maximum-sum-with-at-most-k-elements.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
     long long maxSum(vector<vector<int>>& grid, vector<int>& limits, int k) {
-        priority_queue<int,vector<int>,greater<int>>pq;
-        int index=0;
-        for(auto &row:grid){
-            sort(row.rbegin(),row.rend());
-            for(int i=0;i<row.size()&&i<limits[index];i++){
-                pq.push(row[i]);
-                if(pq.size()>k){
-                    pq.pop();
-                }
+        priority_queue<int> pq;
+        for (int i=0;i<grid.size();i++) {
+            sort(grid[i].rbegin(), grid[i].rend());
+            for (int j = 0; j < grid[i].size() && j < limits[i]; j++) {
+                pq.push(grid[i][j]);
             }
-            index++;
         }
-        long long ans=0;
-        while(pq.size()&&k){
-            ans+=pq.top();
+        long long ans = 0;
+        while (pq.size() &&k--) {
+            ans += pq.top();
             pq.pop();
-            k--;
         }
         return ans;
     }
