719d45f33a06df88fb590c51640cb43583400821
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2025-03-06 00:02:38 +0530

    Time: 93 ms (19.57%), Space: 61 MB (63.8%) - LeetHub

--- 2478-longest-nice-subarray/2478-longest-nice-subarray.cpp
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.cpp
@@ -1,37 +1,29 @@
-class Solution
-{
-    public:
-    int n;
-    bool check(vector<long long>&bit)
-    {
-        for(int i=0;i<32;i++)
-        {
-            if(bit[i]>=2)
-            {
+class Solution {
+public:
+    bool check(vector<long long>& bit) {
+        for (int i = 0; i < 32; i++) {
+            if (bit[i] >= 2) {
                 return false;
             }
         }
         return true;
     }
-    void update(vector<long long>&bit, int num, int value){
-        for(int i=0;i<32;i++){
-            if(num&(1<<i)){
-                bit[i]+=value;
+    void update(vector<long long>& bit, int num, int value) {
+        for (int i = 0; i < 32; i++) {
+            if (num & (1 << i)) {
+                bit[i] += value;
             }
         }
     }
-    int longestNiceSubarray(vector<int> &nums)
-    {
-        n = nums.size();
+    int longestNiceSubarray(vector<int>& nums) {
+        int n = nums.size();
         int j = 0;
         int ans = 1;
-        vector < long long > bit(32, 0);
-        for (int i = 0; i < n; i++)
-        {
-            update(bit,nums[i],1);
-            while (j <= i && check(bit) == false)
-            {
-                update(bit,nums[j],-1);
+        vector<long long> bit(32, 0);
+        for (int i = 0; i < n; i++) {
+            update(bit, nums[i], 1);
+            while (j <= i && check(bit) == false) {
+                update(bit, nums[j], -1);
                 j++;
             }
             ans = max(ans, i - j + 1);
