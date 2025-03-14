c262e6322afc898fd3d7ada6ec4f25288034369a
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2025-03-14 18:52:30 +0530

    Time: 9 ms (57.94%), Space: 10.8 MB (95.32%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -3,19 +3,12 @@ public:
     int numberOfSubstrings(string s) {
         int ans=0;
         int size=s.size();
-        int a=0,b=0,c=0;
+        int last[5];
+        memset(last,-1,sizeof(last));
         int j=0;
         for(int i=0;i<size;i++){
-            if(s[i]=='a') a++;
-            if(s[i]=='b') b++;
-            if(s[i]=='c') c++;
-            while(a>0&&b>0&&c>0){
-                ans+=size-i;
-                if(s[j]=='a') a--;
-                if(s[j]=='b') b--;
-                if(s[j]=='c') c--;
-                j++;
-            }
+            last[s[i]-'a']=i;
+            ans+=1+min({last[0],last[1],last[2]});
         }
         return ans;
     }
