1b0343913414e23fdbd509821c47147d4366dd1e
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2025-03-16 17:49:59 +0530

    Time: 21 ms (83.62%), Space: 59.4 MB (45.76%) - LeetHub

---
+++ 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool find(vector<int>&nums,long long time,int cars){
+        for(int i=0,n=nums.size();i<n;i++){
+            long long temp=sqrt(time/nums[i]);
+            cars-=temp;
+            if(cars<=0) return true;
+        }
+        return false;
+    }
+    long long repairCars(vector<int>& ranks, int cars) {
+        long long l=0;
+        long long r=1e18;
+        while(l<=r){
+            long long mid=(l+r)/2;
+            if(find(ranks,mid,cars)){
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return l;
+    }
+};
