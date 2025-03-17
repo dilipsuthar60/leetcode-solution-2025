ad2249da907e566881b7d62a3e96681d6787ad88
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2025-03-17 14:41:18 +0530

    Time: 8 ms (62.17%), Space: 292.9 MB (69.01%) - LeetHub

--- 3639-zero-array-transformation-i/3639-zero-array-transformation-i.cpp
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
-        int n=nums.size();
-        vector<int>prefix(n+1,0);
-        for(auto &query:queries){
+        int n = nums.size();
+        vector<int> prefix(n + 1, 0);
+        for (auto& query : queries) {
             prefix[query[0]]++;
-            prefix[query[1]+1]--;
+            prefix[query[1] + 1]--;
         }
-        for(int i=1;i<n;i++){
-            prefix[i]+=prefix[i-1];
+        for (int i = 1; i < n; i++) {
+            prefix[i] += prefix[i - 1];
         }
-        for(int i=0;i<n;i++){
-            if(nums[i]>prefix[i]){
-                 return false;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > prefix[i]) {
+                return false;
             }
         }
         return true;
