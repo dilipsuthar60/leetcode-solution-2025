412489b786ff448b13029e53a02843d963cbc1b3
dilipsuthar60 <64765191+dilipsuthar60@users.noreply.github.com>
2025-03-19 14:44:00 +0530

    Time: 22 ms (88.48%), Space: 16 MB (95.86%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<pair<int,int>>direction={{-1,0},{1,0},{0,-1},{0,1}};
+    void find(vector<vector<char>>&grid,int i,int j){
+        if(i<0||j<0||(i>=grid.size())||(j>=grid[0].size())||grid[i][j]=='0'){
+            return ;
+        }
+        grid[i][j]='0';
+        for(auto &it:direction){
+            int nx =i+it.first;
+            int ny =j+it.second;
+            find(grid,nx,ny);
+        }
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int count=0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]=='1'){
+                    find(grid,i,j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
