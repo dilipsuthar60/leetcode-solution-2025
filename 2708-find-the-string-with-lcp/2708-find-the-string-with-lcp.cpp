a2b0e5d4fa4783444fe637b78d900efa040ff9ad
dilipsuthar17 <64765191+dilipsuthar60@users.noreply.github.com>
2025-04-02 14:35:19 +0530

    Time: 15 ms (45.87%), Space: 84.9 MB (22.02%) - LeetHub

---
+++ 2708-find-the-string-with-lcp/2708-find-the-string-with-lcp.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string findTheString(vector<vector<int>>& lcp) {
+        int n=lcp.size();
+        string ans;
+        for(int i=0;i<n;i++) ans+='a';
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(lcp[i][j]==0&&(ans[i]==ans[j])){
+                    ans[j]++;
+                    if(ans[j]>'z'){
+                        return "";
+                    }
+                }
+            }
+        }
+
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int i=n-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(ans[i]==ans[j]){
+                    dp[i][j]=dp[i+1][j+1]+1;
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(lcp[i][j]!=dp[i][j]){
+                    return "";
+                }
+            }
+        }
+        return ans;   
+    }
+};
