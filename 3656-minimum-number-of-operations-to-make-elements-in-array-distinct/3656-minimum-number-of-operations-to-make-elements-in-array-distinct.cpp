39bef42b5e4237311bd1a0e9ced198a8f7710ea2
dilipsuthar17 <64765191+dilipsuthar60@users.noreply.github.com>
2025-04-08 13:34:53 +0530

    Time: 0 ms (100%), Space: 28.3 MB (35.5%) - LeetHub

---
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -0,0 +1,48 @@
+class Solution
+{
+    public: int minimumOperations(vector<int> &nums)
+    {
+        int n = nums.size();
+        unordered_map<int, int> mp;
+        for (auto &num: nums) mp[num]++;
+        if (mp.size() == n)
+        {
+            return 0;
+        }
+        int count = 0;
+        int removeFirstElement = 0;
+        for (int i = 0; i < n; i += 3)
+        {
+            mp[nums[i]]--;
+            count++;
+            if (mp[nums[i]] == 0)
+            {
+                mp.erase(nums[i]);
+            }
+            removeFirstElement++;
+            if (i + 1 < n)
+            {
+                mp[nums[i + 1]]--;
+                if (mp[nums[i + 1]] == 0)
+                {
+                    mp.erase(nums[i + 1]);
+                }
+                removeFirstElement++;
+            }
+            if (i + 2 < n)
+            {
+                mp[nums[i + 2]]--;
+                if (mp[nums[i + 2]] == 0)
+                {
+                    mp.erase(nums[i + 2]);
+                }
+                removeFirstElement++;
+            }
+            if (mp.size() == (n - removeFirstElement))
+            {
+                return count;
+            }
+        }
+        return count;
+    }
+};
