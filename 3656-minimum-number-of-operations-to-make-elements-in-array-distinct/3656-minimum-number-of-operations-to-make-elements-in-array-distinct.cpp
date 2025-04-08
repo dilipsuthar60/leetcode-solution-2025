d342efc9c97da34920de6cf3df8b565ac6b30ba5
dilipsuthar17 <64765191+dilipsuthar60@users.noreply.github.com>
2025-04-08 13:43:56 +0530

    Time: 1 ms (65.36%), Space: 27.7 MB (66.04%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -3,46 +3,13 @@ class Solution
     public: int minimumOperations(vector<int> &nums)
     {
         int n = nums.size();
-        unordered_map<int, int> mp;
-        for (auto &num: nums) mp[num]++;
-        if (mp.size() == n)
-        {
-            return 0;
+        unordered_set<int>s;
+        for(int i=n-1;i>=0;i--){
+            if(s.find(nums[i])!=s.end()){
+                return i/3+1;
+            }
+            s.insert(nums[i]);
         }
-        int count = 0;
-        int removeFirstElement = 0;
-        for (int i = 0; i < n; i += 3)
-        {
-            mp[nums[i]]--;
-            count++;
-            if (mp[nums[i]] == 0)
-            {
-                mp.erase(nums[i]);
-            }
-            removeFirstElement++;
-            if (i + 1 < n)
-            {
-                mp[nums[i + 1]]--;
-                if (mp[nums[i + 1]] == 0)
-                {
-                    mp.erase(nums[i + 1]);
-                }
-                removeFirstElement++;
-            }
-            if (i + 2 < n)
-            {
-                mp[nums[i + 2]]--;
-                if (mp[nums[i + 2]] == 0)
-                {
-                    mp.erase(nums[i + 2]);
-                }
-                removeFirstElement++;
-            }
-            if (mp.size() == (n - removeFirstElement))
-            {
-                return count;
-            }
-        }
-        return count;
+        return 0;
     }
 };
