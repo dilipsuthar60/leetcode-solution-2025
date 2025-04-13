fec929d0fa11dc2541f4544afb22909e8d83fe3e
dilipsuthar17 <64765191+dilipsuthar60@users.noreply.github.com>
2025-04-13 19:48:50 +0530

    Time: 20 ms (100%), Space: 61 MB (68.42%) - LeetHub

--- 3812-smallest-palindromic-rearrangement-i/3812-smallest-palindromic-rearrangement-i.cpp
+++ 3812-smallest-palindromic-rearrangement-i/3812-smallest-palindromic-rearrangement-i.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     string smallestPalindrome(string s) {
-        int n=s.size();
-        vector<int>dp(26,0);
-        for(auto &ch:s) dp[ch-'a']++;
-        int index=0;
-        string ans = string(n,'#');
+        int n = s.size();
+        vector<int> dp(26, 0);
+        for (auto& ch : s)
+            dp[ch - 'a']++;
+        int index = 0;
         char mid='#';
-        for(int i=0;i<26;i++){
-            while(dp[i]>=2){
-                ans[index]=char('a'+i);
-                ans[n-index-1]=char('a'+i);
+        string ans = string(n, '#');
+        for (int i = 0; i < 26; i++) {
+            while (dp[i] >= 2) {
+                ans[index] = char('a' + i);
+                ans[n - index - 1] = char('a' + i);
                 index++;
-                dp[i]-=2;
+                dp[i] -= 2;
             }
-        }
-        for(int i=0;i<26;i++){
             if(dp[i]){
-                ans[index]=char('a'+i);
-                break;
+                mid=char('a' + i);
             }
         }
+
+        if(mid!='#') ans[index] = mid;
+
         return ans;
     }
 };
