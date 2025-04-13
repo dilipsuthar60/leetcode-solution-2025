298c05c5a2173399a6f2321b0a7afa67e9390245
dilipsuthar17 <64765191+dilipsuthar60@users.noreply.github.com>
2025-04-13 20:02:24 +0530

    Time: 2378 ms (51.43%), Space: 100.2 MB (74.29%) - LeetHub

---
+++ 3820-number-of-unique-xor-triplets-ii/3820-number-of-unique-xor-triplets-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        unordered_set<int>twoNumberXor;
+        unordered_set<int>threeNumberXor;
+        int n=nums.size();
+        if(n==1||n==2) return n;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                twoNumberXor.insert(nums[i]^nums[j]);
+            }
+        }
+
+        for(auto &it:twoNumberXor){
+            for(int i=0;i<n;i++){
+                threeNumberXor.insert(it^nums[i]);
+            }
+        }
+        return threeNumberXor.size();
+    }
+};
