ae5699d34944af31f2c6760c0c31dffe3842ae73
dilipsuthar17 <64765191+dilipsuthar60@users.noreply.github.com>
2025-04-27 20:11:23 +0530

    Time: 0 ms (100%), Space: 48.3 MB (75.3%) - LeetHub

--- 3685-count-subarrays-of-length-three-with-a-condition/3685-count-subarrays-of-length-three-with-a-condition.cpp
+++ 3685-count-subarrays-of-length-three-with-a-condition/3685-count-subarrays-of-length-three-with-a-condition.cpp
@@ -4,9 +4,7 @@ public:
         int count=0;
         int n=nums.size();
         for(int i=0;i<n-2;i++){
-            if((nums[i]+nums[i+2])*2==nums[i+1]){
-                count++;
-            }
+            count+=((nums[i]+nums[i+2])*2==nums[i+1]);
         }
         return count;
     }
