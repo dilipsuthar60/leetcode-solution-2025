c89ead326b0f557f2264509b35266a8304ceb443
dilipsuthar17 <64765191+dilipsuthar60@users.noreply.github.com>
2025-05-03 21:40:33 +0530

    Time: 114 ms (17.21%), Space: 79.5 MB (21.64%) - LeetHub

---
+++ 2742-group-by/2742-group-by.js
@@ -0,0 +1,18 @@
+/**
+ * @param {Function} fn
+ * @return {Object}
+ */
+Array.prototype.groupBy = function(fn) {
+    return this.reduce((groupItem, item)=>{
+        const key = fn(item);
+        if(!groupItem[key]){
+            groupItem[key] = []
+        }
+        groupItem[key].push(item);
+        return groupItem;
+    }, {})
+};
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
