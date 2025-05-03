ce8cfbb4fa58c24fd085800acdfe4144614d5540
dilipsuthar17 <64765191+dilipsuthar60@users.noreply.github.com>
2025-05-03 20:51:13 +0530

    Time: 4 ms (56.25%), Space: 115.4 MB (76.51%) - LeetHub

--- 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.cpp
+++ 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.cpp
@@ -1,51 +1,18 @@
 class Solution {
 public:
-    int find(vector<int>&t,vector<int>&b,int val)
-    {
-        int n=t.size();
-        int count1=0;
-        int count2=0;
-        for(int i=0;i<n;i++)
-        {
-            if(count1!=INT_MAX)
-            {
-                if(val==t[i])
-                {
-                }
-                else if(val==b[i])
-                {
-                    count1++;
-                }
-                else
-                {
-                    count1=INT_MAX;
-                }
-            }
-            if(count2!=INT_MAX)
-            {
-                if(val==b[i])
-                {
-                }
-                else if(val==t[i])
-                {
-                    count2++;
-                }
-                else
-                {
-                    count2=INT_MAX;
-                }
-            }
+    int find(vector<int>&nums1,vector<int>&nums2,int value){
+        int counter1=0, counter2=0;
+        for(int i=0;i<nums1.size();i++){
+            if((nums1[i]!=value)&&(nums2[i]!=value)) 
+                return INT_MAX;
+            if(nums1[i]!=value) counter1++;
+            if(nums2[i]!=value) counter2++;
         }
-        return min(count1,count2);
+        return min(counter1,counter2);
     }
-    int minDominoRotations(vector<int>& tops, vector<int>& bottoms) 
-    {
-        int ans1=find(tops,bottoms,tops[0]);
-        int ans2=find(tops,bottoms,bottoms[0]);
-        if(ans1==INT_MAX&&ans2==INT_MAX)
-        {
-            return -1;
-        }
-        return min(ans1,ans2);
+    int minDominoRotations(vector<int>& nums1, vector<int>& nums2) {
+        int operation1=find(nums1,nums2,nums1.front());
+        int operation2=find(nums1,nums2,nums2.front());
+        return (min(operation1,operation2)==INT_MAX)?-1:min(operation1,operation2);
     }
 };
