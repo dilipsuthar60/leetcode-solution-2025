2099418df72e3dc252662330cafb638b14a6cf02
dilipsuthar17 <64765191+dilipsuthar60@users.noreply.github.com>
2025-07-01 02:04:53 +0530

    Time: 530 ms (62.51%), Space: 32.4 MB (97.71%) - LeetHub

--- 3913-partition-array-to-minimize-xor/3913-partition-array-to-minimize-xor.cpp
+++ 3913-partition-array-to-minimize-xor/3913-partition-array-to-minimize-xor.cpp
@@ -3,13 +3,13 @@ public:
     int dp[255][255];
     int find(vector<int>&nums,int index,int k){
         if(index>=nums.size()||k<0){
-            return (k==0)?0:INT_MAX-1;
+            return (k==0)?0:INT_MAX;
         }
         if(dp[index][k]!=-1){
             return dp[index][k];
         }
         int currentXor=0;
-        int ans=INT_MAX-1;
+        int ans=INT_MAX;
         for(int i=index;i<nums.size();i++){
             currentXor^=nums[i];
             ans=min(ans, max(currentXor, find(nums,i+1,k-1)));
@@ -18,7 +18,6 @@ public:
     }
     int minXor(vector<int>& nums, int k) {
         memset(dp,-1,sizeof(dp));
-        int value=find(nums,0,k);
-        return value;
+        return find(nums,0,k);
     }
 };
