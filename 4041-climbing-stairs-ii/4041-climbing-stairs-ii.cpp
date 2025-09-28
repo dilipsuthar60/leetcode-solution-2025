d016b5819ae63a1c6f02e18e19e98252e9ff288d
dilipsuthar17 <64765191+dilipsuthar60@users.noreply.github.com>
2025-09-28 22:48:52 +0530

    Time: 44 ms (15.6%), Space: 188.7 MB (12.5%) - LeetHub

--- 4041-climbing-stairs-ii/4041-climbing-stairs-ii.cpp
+++ 4041-climbing-stairs-ii/4041-climbing-stairs-ii.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     vector<int>dp;
-    int getCost(int a, int b){
-        int value=abs(a-b);
-        return value*value;
-    }
     int find(vector<int>&nums, int index, int n){
         if(index>=n) return 0;
         int ans=1e9;
         if(dp[index]!=-1) return dp[index];
         if(index<n){
-            ans=min(ans, getCost(index,index+1)+nums[index]+find(nums,index+1, n));
+            ans=min(ans, 1+nums[index]+find(nums,index+1, n));
         }
         if(index+1<n){
-            ans=min(ans, getCost(index,index+2)+nums[index+1]+find(nums,index+2, n));
+            ans=min(ans, 4+nums[index+1]+find(nums,index+2, n));
         }
         if(index+2<n){
-            ans=min(ans, getCost(index,index+3)+nums[index+2]+find(nums,index+3, n));
+            ans=min(ans, 9+nums[index+2]+find(nums,index+3, n));
         }
         return dp[index]= ans;
     }
