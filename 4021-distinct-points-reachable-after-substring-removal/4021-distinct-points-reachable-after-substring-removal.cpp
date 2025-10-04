4217dab8a32de0b0547bc33561174fc52969a3a2
dilipsuthar17 <64765191+dilipsuthar60@users.noreply.github.com>
2025-10-04 21:20:25 +0530

    Time: 129 ms (54.91%), Space: 53.3 MB (53.09%) - LeetHub

--- 4021-distinct-points-reachable-after-substring-removal/4021-distinct-points-reachable-after-substring-removal.cpp
+++ 4021-distinct-points-reachable-after-substring-removal/4021-distinct-points-reachable-after-substring-removal.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
+    pair<int,int>getMovePosition(char ch){
+        if(ch=='U') return {0,1};
+        if(ch=='D') return {0,-1};
+        if(ch=='L') return {-1, 0};
+        return {1, 0};
+    }
     int distinctPoints(string s, int k) {
         int n=s.size();
         vector<pair<int,int>>prefix(n+1, {0,0});
         for(int i=1;i<=n;i++){
-            prefix[i].first = prefix[i-1].first;
-            prefix[i].second = prefix[i-1].second;
-            if(s[i-1]=='U'){
-                prefix[i].second++;
-            }
-            else if(s[i-1]=='D'){
-                prefix[i].second--;
-            }
-            else if(s[i-1]=='L'){
-                prefix[i].first--;
-            }
-            else{
-                prefix[i].first++;
-            }
+            auto position=getMovePosition(s[i-1]);
+            prefix[i].first = prefix[i-1].first+position.first;
+            prefix[i].second = prefix[i-1].second+position.second;
         }
         int totalX=prefix.back().first;
         int totalY=prefix.back().second;
