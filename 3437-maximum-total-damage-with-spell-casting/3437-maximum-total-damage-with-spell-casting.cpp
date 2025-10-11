fde8acc1a8b1efcd26ed9980ac92aba1be0821ec
dilipsuthar17 <64765191+dilipsuthar60@users.noreply.github.com>
2025-10-11 22:47:38 +0530

    Time: 361 ms (45.41%), Space: 224.5 MB (26.52%) - LeetHub

---
+++ 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    long long dp[100004];
+    unordered_map<long long,long long>mp;
+    vector<long long>nums;
+    long long find(int index)
+    {
+        if(index>=nums.size()) return 0;
+        if(dp[index]!=-1) return dp[index];
+        long long notTake=find(index+1);
+        long long take=0;
+        int nextIndex=lower_bound(nums.begin(),nums.end(),nums[index]+3)-nums.begin();
+        take=mp[nums[index]]*nums[index]+find(nextIndex);
+        return dp[index]= max(take,notTake);
+    }
+    long long maximumTotalDamage(vector<int>& power) {
+        for(auto &it:power) mp[it]++;
+        for(auto &[key,value]:mp) nums.push_back(key);
+        memset(dp,-1,sizeof(dp));
+        sort(nums.begin(),nums.end());
+        return find(0);
+    }
+};
