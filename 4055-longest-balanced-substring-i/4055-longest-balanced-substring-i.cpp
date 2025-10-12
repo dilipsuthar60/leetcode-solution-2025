12f66c38b20bb0acab7839f7aaa3b189ca902f54
dilipsuthar17 <64765191+dilipsuthar60@users.noreply.github.com>
2025-10-12 21:54:52 +0530

    Time: 42 ms (100%), Space: 11.5 MB (75%) - LeetHub

--- 4055-longest-balanced-substring-i/4055-longest-balanced-substring-i.cpp
+++ 4055-longest-balanced-substring-i/4055-longest-balanced-substring-i.cpp
@@ -5,21 +5,11 @@ public:
         int ans=1;
         for(int i=0;i<n;i++){
             int dp[26]={0};
+            int maxFreq=0, uniqueCharCount=0;
             for(int j=i;j<n;j++){
-                dp[s[j]-'a']++;
-                int first=-1;
-                int isValid=true;
-                for(int k=0;k<26;k++){
-                    if(dp[k]){
-                        if(first==-1){
-                            first=dp[k];
-                        }
-                        else if(first!=dp[k]){
-                            isValid=false;
-                        }
-                    }
-                }
-                if(isValid){
+                maxFreq=max(maxFreq, ++dp[s[j]-'a']);
+                if(dp[s[j]-'a']==1) uniqueCharCount++;
+                if(maxFreq*uniqueCharCount==(j-i+1)){
                     ans=max(ans,j-i+1);
                 }
             }
