3592d706c03f2b1b4cff7e6389d7aa5cd28dffe2
dilipsuthar17 <64765191+dilipsuthar60@users.noreply.github.com>
2025-10-25 09:42:10 +0530

    Time: 107 ms (2.7%), Space: 7.9 MB (53.24%) - LeetHub

---
+++ 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int totalMoney(int n) {
+        int amount=0;
+        for(int i=0, start=0, startPoint=0;i<n;i++){
+            if(i%7==0){
+                startPoint++;
+                start=startPoint;
+            }
+            cout<<start<<".   ";
+            amount+=start;
+            start++;
+        }
+        return amount;
+    }
+};
