6439b12d525610366a5819de373560fe25040021
dilipsuthar17 <64765191+dilipsuthar60@users.noreply.github.com>
2025-10-30 09:51:48 +0530

    Time: 0 ms (100%), Space: 76.8 MB (87.87%) - LeetHub

--- 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
+++ 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int minNumberOperations(vector<int>& target) {
-        int ans=0;
+        int ans=target.front();
         for(int i=1;i<target.size();i++){
             ans+=max(0,target[i]-target[i-1]);
         }
-        return ans+target.front();
+        return ans;
     }
 };
