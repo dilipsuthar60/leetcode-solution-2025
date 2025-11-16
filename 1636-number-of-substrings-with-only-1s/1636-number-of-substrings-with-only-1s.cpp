5da30c415339ea30d58d1e2ec2e4baf504c03012
dilipsuthar17 <64765191+dilipsuthar60@users.noreply.github.com>
2025-11-16 18:14:18 +0530

    Time: 2 ms (39.33%), Space: 11.5 MB (95.37%) - LeetHub

--- 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.cpp
+++ 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.cpp
@@ -5,14 +5,8 @@ public:
         int n=s.size();
         int one=0, result=0;
         for(int i=0;i<n;i++){
-            if(s[i]=='1'){
-                one++;
-            }
-            else{
-                one=0;
-            }
-            result+=one;
-            result%=mod;
+            one=(s[i]=='1')?one+1:0;
+            result=(result+one)%mod;
         }
         return result;
     }
