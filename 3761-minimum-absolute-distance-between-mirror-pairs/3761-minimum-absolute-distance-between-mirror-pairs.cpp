28a7465eed4db3c575c0aec23814b3cc7acd1dd9
dilipsuthar17 <64765191+dilipsuthar60@users.noreply.github.com>
2025-11-30 21:28:36 +0530

    Time: 116 ms (66.67%), Space: 131.8 MB (44.44%) - LeetHub

---
+++ 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int reverse(int n){
+        int rev=0;
+        while(n){
+            rev=rev*10+n%10;
+            n/=10;
+        }
+        return rev;
+    }
+    int minMirrorPairDistance(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        int ans=1e9,n=nums.size();
+        for(int i=0;i<n;i++){
+            int rev=reverse(nums[i]);
+            if(mp.find(nums[i])!=mp.end()){
+                ans=min(ans,abs(i-mp[nums[i]]));
+            }
+            mp[rev]=i;
+        }
+        return ans==1e9?-1:ans;
+    }
+};
