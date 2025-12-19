d0695d5374ae38b52728a61d5e9a1978f521e811
dilipsuthar17 <64765191+dilipsuthar60@users.noreply.github.com>
2025-12-20 01:18:30 +0530

    Time: 209 ms (51.63%), Space: 210.1 MB (38.05%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
@@ -1,42 +1,31 @@
 class Solution {
 public:
-    vector<pair<int,int>>graph[200005]; // child and weight
-    vector<int> findAllPeople(int n, vector<vector<int>>&nums, int person) 
-    {
-        // graph creation
-        for(auto &it:nums)
-        {
-            graph[it[0]].push_back({it[1],it[2]});
-            graph[it[1]].push_back({it[0],it[2]});
+    vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
+        vector<pair<int,int>>dp[n+1];
+        for(auto &meeting:meetings){
+            dp[meeting[0]].push_back({meeting[1],meeting[2]});
+            dp[meeting[1]].push_back({meeting[0],meeting[2]});
         }
+        vector<bool>visited(n,0);
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
         pq.push({0,0});
-        pq.push({0,person});
-        vector<int>visited(n,0);
-        while(pq.size())
-        {
-            auto [time,node]=pq.top();
+        pq.push({0,firstPerson});
+        while(pq.size()){
+            auto [time,parentNode]=pq.top();
             pq.pop();
-            if(visited[node])
-            {
+            if(visited[parentNode]){
                 continue;
             }
-            visited[node]=1;
-            for(auto [currentNode,currentTime]:graph[node])
-            {
-                if(time<=currentTime&&visited[currentNode]==0)
-                {
-                    pq.push({currentTime,currentNode});
+            visited[parentNode]=true;
+            for(auto &[childNode, currentTime]:dp[parentNode]){
+                if(time<=currentTime){
+                    pq.push({currentTime, childNode});
                 }
             }
         }
         vector<int>result;
-        for(int index=0;index<n;index++)
-        {
-            if(visited[index])
-            {
-                result.push_back(index);
-            }
+        for(int i=0;i<n;i++){
+            if(visited[i]) result.push_back(i);
         }
         return result;
     }
